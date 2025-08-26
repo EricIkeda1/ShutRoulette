86396e2e4bb05255ecf0015d452b3bdd9b7ae718
EricIkeda1 <93358246+EricIkeda1@users.noreply.github.com>
2025-08-26 12:39:00 -0300

    Update shutroulette.py

--- shutroulette.py
+++ shutroulette.py
@@ -3,20 +3,16 @@ import shutil
 import os
 
 def download_exe():
-    # Caminho do executável gerado
     arquivo_exe = "dist\\shutroulette.exe"
     
     if os.path.exists(arquivo_exe):
-        # Defina o local para salvar o arquivo
-        destino = os.path.expanduser('~') + "\\Downloads\\shutroulette.exe"  # Baixar para a pasta Downloads
+        destino = os.path.expanduser('~') + "\\Downloads\\shutroulette.exe"
         
-        # Copia o executável para a pasta de downloads
         shutil.copy(arquivo_exe, destino)
         label_var.set(f"Executável baixado com sucesso! Salvo em: {destino}")
     else:
         label_var.set("Arquivo não encontrado. Certifique-se de que o .exe foi gerado.")
 
-# Criando a interface gráfica
 root = tk.Tk()
 root.title("Baixar Executável")
 root.geometry("400x150")
@@ -31,3 +27,4 @@ botao = tk.Button(root, text="Baixar .exe", command=download_exe)
 botao.pack()
 
 root.mainloop()
+
